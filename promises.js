4d28f51def5d186a3ab03a99c60d91d23f8f54fc
Marcelofury <marcelofury13579@gmail.com>
2026-06-18 14:53:56 +0300

    promise definition

---
+++ promises.js
@@ -0,0 +1,5 @@
+/**
+ * PROMISES are another way to use callback functions.Rather than integrating the method call (as a parameter), we use it as a parameter of the new then(callback) method.This simplifies the reading of Javascript code in case it uses callback functions.
+ */
+
+
